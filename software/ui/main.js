780605e0396f449d3f3e14e9cfc6f9e96b4cc2b3
Lupu Cristian <cristi.a.lupu@gmail.com>
2016-02-21 16:32:45 +0200

    electron file update

--- software/ui/main.js
+++ software/ui/main.js
@@ -12,13 +12,16 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600, resizable:false, movable:false, closable:true, skipTaskbar:true,
+	title:"Leoni cable test", frame:false
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  //mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL("http://localhost:3000");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
